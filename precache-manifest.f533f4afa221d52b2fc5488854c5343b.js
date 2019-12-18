d9639e6c472a38bdabd89b31ce793ced9f0bac7e
ugu <yinyun957@gmail.com>
2019-12-18 10:32:39 +0000

    Travis CI Auto Builder :2019-12-18 18:32:39

---
+++ precache-manifest.f533f4afa221d52b2fc5488854c5343b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f16c17402d655319e42837ca16dbddeb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b6762d457e40423f332b",
+    "url": "./static/css/2.c45579fc.chunk.css"
+  },
+  {
+    "revision": "b6762d457e40423f332b",
+    "url": "./static/js/2.72f0f665.chunk.js"
+  },
+  {
+    "revision": "4ca57b6f66f969d394ef185db56928b6",
+    "url": "./static/js/2.72f0f665.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cffb183e9c6a2d4415dd",
+    "url": "./static/js/main.4409deae.chunk.js"
+  },
+  {
+    "revision": "15e4467a79572d20e78f",
+    "url": "./static/js/runtime-main.7fdcfca4.js"
+  }
+]);
